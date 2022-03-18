059e63e1bf25b4c2f8c5c9388a70b794946eacf1
amirjelodarian <amirjelodarian@gmail.com>
2022-03-19 02:20:17 +0330

    somthing change

--- app/Utilities.php
+++ app/Utilities.php
@@ -8,4 +8,35 @@
         if ($EnOrFa == 'fa')
             return str_replace($en, $fa, $number);
     }
+
+    function rndAnim(){
+        $fadeName = [
+            'fade-up',
+            'fade-down',
+            'fade-right',
+            'fade-left',
+            'fade-up-right',
+            'fade-up-left',
+            'fade-down-right',
+            'fade-down-left',
+            'flip-left',
+            'flip-right',
+            'flip-up',
+            'flip-down',
+            'zoom-in',
+            'zoom-in-up',
+            'zoom-in-up',
+            'zoom-in-left',
+            'zoom-in-right',
+            'zoom-out',
+            'zoom-out-up',
+            'zoom-out-down',
+            'zoom-out-right',
+            'zoom-out-left',
+            'fade-zoom-in'
+        ];
+        $aniName = '';
+        while($aniName == '')
+            $aniName = $fadeName[rand(0,count($fadeName))];
+    }
 ?>
--- resources/views/layouts/productTemplate.blade.php
+++ resources/views/layouts/productTemplate.blade.php
@@ -1,5 +1,5 @@
 @foreach ($products as $product)
-<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2 product" style="margin-top: 25px">
+<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2 product" style="margin-top: 25px" data-aos="{{ rndAnim() }}">
     <div class="product-inside">
         <div class="margin-b-10">
             <a class="product-img-box" href="{{ route('product.show', $product->id) }}">
@@ -7,7 +7,9 @@
                 <img class="img-responsive product-img" src={{ $product->photo->fullPath() }} alt="Latest Products Image">
             </a>
         </div>
-        <p class="product-title"><a href="{{ route('product.show', $product->id) }}">{{ $product->title  }}</a></p>
+        <p class="product-title">
+            <a href="{{ route('product.show', $product->id) }}">{{ $product->title  }}</a>
+        </p>
         <p class="product-car">
             @foreach($product->categories as $category)
                 ({{ $category->name }})
